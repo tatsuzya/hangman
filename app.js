296f6fc1067d7ace7ca2f03f7cf8226e9fd69093
tatsuzya <jay_820028@hotmail.com>
2018-11-02 21:39:41 -0700

    added ranking page. more fix to functionalities

--- app.js
+++ app.js
@@ -21,9 +21,14 @@
   const newUser = document.getElementById('new');
   const oldUser = document.getElementById('exist');
   const user = document.getElementById('user');
+  var unsortName = new Array();
+  var sortScore = new Array();
+  var unsortScore = new Array();
+  var sortName = new Array();
 
-  // function for test.html
-  if (window.location.href.match('test.html') != null) {
+
+  // function for index.html
+  if (window.location.href.match('index.html') != null) {
 
     // add new event
     newUser.addEventListener('click', e => {
@@ -32,6 +37,7 @@
       btnSignup.style.display = "block";
       oldUser.style.display = "block";
       user.style.display = "block";
+      document.getElementById('msg').innerHTML = "";
       e.preventDefault();
     });
 
@@ -42,6 +48,7 @@
       btnSignup.style.display = "none";
       newUser.style.display = "block";
       user.style.display = "none";
+      document.getElementById('msg').innerHTML = "";
       e.preventDefault();
     });
 
@@ -52,6 +59,9 @@
       const auth = firebase.auth();
 
       const promise = auth.signInWithEmailAndPassword(email, pass);
+      promise.then(e => {
+        window.location.href = "hangman.html";
+      });
       promise.catch(e => document.getElementById('msg').innerHTML = "" + e.message);
     });
 
@@ -66,9 +76,15 @@
         var user = firebase.auth().currentUser;
         user.updateProfile({
           displayName: "" + username,
-        })
+        });
+        firebase.database().ref('users/' + user.uid).set({
+          name: "" + username,
+          score: "0"
+        });
+        document.getElementById('msg').innerHTML = "" + successMsg;
       });
       promise.catch(e => document.getElementById('msg').innerHTML = "" + e.message);
+      firebase.auth().signOut();
     });
   }
 
@@ -94,32 +110,62 @@
     });
   }
 
-  if(window.location.href.match('ranking.html')!= null){
-    
+  if (window.location.href.match('ranking.html') != null) {
+    var query = firebase.database().ref("users/").orderByKey();
+    var i = 0;
+    var title = document.getElementById('rank-page');
+    var panel = document.getElementById('rankpanel');
+
+    query.once("value")
+      .then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var rankName = childSnapshot.val().name;
+          unsortName[i] = rankName;
+          var rankScore = childSnapshot.val().score;
+          sortScore[i] = rankScore;
+          unsortScore[i] = rankScore;
+          i++;
+        });
+        sortScore.sort((a, b) => b - a);
+        for (let j = 0; j < sortScore.length; j++) {
+          for(let k = 0; k < sortScore.length; k++){
+            if(sortScore[j] == unsortScore[k]){
+              sortName[j] = unsortName[k];
+            }
+          }
+        }
+        for(let l = 0; l < sortScore.length-1; l++){
+          title.appendChild(panel.cloneNode(true));
+        }
+
+        var rName = document.querySelectorAll('#name');
+        var rScore = document.querySelectorAll('#score');
+        var rRank = document.querySelectorAll('#rank');
+
+        for(let x = 0; x < rName.length; x++){
+          rName[x].innerHTML = "" + sortName[x];
+          rScore[x].innerHTML = "" + sortScore[x];
+          rRank[x].innerHTML = "" + (x + 1);
+        }
+      });
   }
 
   // add realtime addEventListener
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
-      console.log("logged in");
-      console.log(firebaseUser.displayName);
-      if (window.location.href.match('test.html') != null) {
-        window.location.href = "hangman.html";
-      }
       if (window.location.href.match('hangman.html') != null) {
         document.getElementById('displayname').innerHTML = "Welcome! " + firebaseUser.displayName;
         var user = firebase.auth().currentUser;
         var ref = firebase.database().ref('users/' + user.uid);
+
         ref.on("value", snapshot => {
-          console.log("" + score);
           score = snapshot.val().score;
           document.getElementById('score').innerHTML = "score: " + score;
         });
       }
     } else {
-      console.log('not logged in');
       if (window.location.href.match('hangman.html') != null) {
-        window.location.href = "test.html";
+        window.location.href = "index.html";
       }
     }
   });
